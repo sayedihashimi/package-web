16043b173dec9007b2b126e41e76237ad027a89e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-03-13 21:25:40 -0700

    Adding samples project

---
+++ Sample/Samples/PkgWebSample/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿namespace PkgWebSample.Controllers {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using PkgWebSample.Models;
+
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            
+            // get app settings
+            HomePageModel hpm = new HomePageModel();
+
+            foreach (var key in ConfigurationManager.AppSettings.AllKeys) {
+                hpm.AppSettings[key] = ConfigurationManager.AppSettings[key];
+            }
+
+            foreach (ConnectionStringSettings conString in ConfigurationManager.ConnectionStrings) {
+                hpm.ConnectionStrings[conString.Name] = conString.ConnectionString;
+            }
+
+            return View(hpm);
+        }
+
+    }
+}
