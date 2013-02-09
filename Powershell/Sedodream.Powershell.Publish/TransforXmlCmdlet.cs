79d91b41f4946597950ca814be322938b8871b40
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-02-09 14:20:45 -0800

    cleaning up code #38

--- Powershell/Sedodream.Powershell.Publish/TransforXmlCmdlet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sedodream.Powershell.Publish {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-
-    public class TransforXmlCmdlet {
-    }
-}
